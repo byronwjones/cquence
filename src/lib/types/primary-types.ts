f6385e18745e97579b74234815a9af3bd33adfe6
Byron Jones <byron@byronwjones.com>
2019-02-04 18:58:27 -0500

    Preparing for TypeScript build

--- src/lib/types/primary-types.ts
+++ src/lib/types/primary-types.ts
@@ -1,12 +1,2 @@
+const privado = (Math.random() * 1000000).toString();
 type NormalMap = {[key:string]: any};
-
-/// <reference path="interfaces/interfaces.ts" />
-
-type UnitFunction = (unitConductor: IConductorInterface) => void;
-type CompositionFunction = (composer: Composer) => void;
-
-type ExecutionTarget = IConductorBuilder | UnitFunction;
-
-type PromiseVirtualFunction = (args: NormalMap, update?: (updateDetail: any) => void) => PromiseLike<any>;
-type CallbackVirtualFunction = (params?: ICallbackVirtualFunctionParameters) => void;
-type VirtualFunction = PromiseVirtualFunction | CallbackVirtualFunction;
